7e0a2266a803a630821a760f58255fdae552a893
Bruce Lin <ruiyangl@andrew.cmu.edu>
2016-11-30 21:05:40 -0500

    added start game scene. Missing start game button image though

--- Punchout/GameViewController.swift
+++ Punchout/GameViewController.swift
@@ -13,43 +13,33 @@ import GameplayKit
 class GameViewController: UIViewController {
 
     override func viewDidLoad() {
+//        super.viewDidLoad()
+//        
+//        if let scene = GameScene(fileNamed:"GameScene") {
+//            // Configure the view.
+//            let skView = self.view as! SKView
+//            skView.showsFPS = true
+//            skView.showsNodeCount = true
+//            
+//            /* Sprite Kit applies additional optimizations to improve rendering performance */
+//            skView.ignoresSiblingOrder = true
+//            
+//            /* Set the scale mode to scale to fit the window */
+//            scene.scaleMode = .AspectFill
+//            
+//            skView.presentScene(scene)
+//        }
         super.viewDidLoad()
-        
-        if let view = self.view as! SKView? {
-            // Load the SKScene from 'GameScene.sks'
-            if let scene = SKScene(fileNamed: "GameScene") {
-                // Set the scale mode to scale to fit the window
-                scene.scaleMode = .aspectFill
-                
-                // Present the scene
-                view.presentScene(scene)
-            }
-            
-            view.ignoresSiblingOrder = true
-            
-            view.showsFPS = true
-            view.showsNodeCount = true
-        }
+        let scene = StartGameScene(size: view.bounds.size)
+        let skView = view as! SKView
+        skView.showsFPS = true
+        skView.showsNodeCount = true
+        skView.ignoresSiblingOrder = true
+        scene.scaleMode = .ResizeFill
+        skView.presentScene(scene)
     }
-
-    override var shouldAutorotate: Bool {
-        return true
-    }
-
-    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
-        if UIDevice.current.userInterfaceIdiom == .phone {
-            return .allButUpsideDown
-        } else {
-            return .all
-        }
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Release any cached data, images, etc that aren't in use.
-    }
-
-    override var prefersStatusBarHidden: Bool {
+    
+    override func prefersStatusBarHidden() -> Bool {
         return true
     }
 }
--- Punchout/StartGameScene.swift
+++ Punchout/StartGameScene.swift
@@ -19,4 +19,16 @@ class StartGameScene: SKScene {
         startGameButton.name = "startgame"
         addChild(startGameButton)
     }
+    
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        let touch = touches.first! as UITouch
+        let touchLocation = touch.locationInNode(self)
+        let touchedNode = self.nodeAtPoint(touchLocation)
+        if touchedNode.name == "startgame" {
+            let gameOverScene = GameScene(size: size)
+            gameOverScene.scaleMode = scaleMode
+            let transitionType = SKTransition.flipHorizontalWithDuration(1.0)
+            view?.presentScene(gameOverScene,transition: transitionType)
+        }
+    }
 }
