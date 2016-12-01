9fc6cb1bb67c16d8eda964b55a4437e6b4520ff9
grubmatt <grubmatt@users.noreply.github.com>
2016-11-30 20:36:56 -0500

    Merge master? (#1)
    * added opponent
    
    * updated language to swift 2 legacy enabled

---
+++ Punchout/fist.swift
@@ -0,0 +1,14 @@
+//
+//  fist.swift
+//  Punchout
+//
+//  Created by Brucey on 11/30/16.
+//  Copyright © 2016 CMU. All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+
+class fist: SKShapeNode {
+    
+}
---
+++ Punchout/opponent.swift
@@ -0,0 +1,9 @@
+//
+//  opponent.swift
+//  Punchout
+//
+//  Created by Matt Gruber on 11/30/16.
+//  Copyright © 2016 CMU. All rights reserved.
+//
+
+import Foundation
